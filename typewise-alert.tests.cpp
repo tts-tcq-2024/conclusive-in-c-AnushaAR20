43aacee771d52c6f2703652be2b579b55418d2bb
AnushaAR20 <167959589+AnushaAR20@users.noreply.github.com>
2024-08-20 10:50:42 +0530

    Update typewise-alert.tests.cpp

--- typewise-alert.tests.cpp
+++ typewise-alert.tests.cpp
@@ -5,5 +5,6 @@ TEST(TypeWiseAlertTestSuite,InfersBreachAccordingToLimits) {
 EXPECT_EQ(inferBreach(12, 10, 20), NORMAL);
 EXPECT_EQ(inferBreach(9, 10, 20), TOO_LOW);
 EXPECT_EQ(inferBreach(21, 10, 20), TOO_HIGH);
+EXPECT_EQ(inferBreach(21, 10, 21), TOO_HIGH);
 
 }
