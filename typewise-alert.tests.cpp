d41ffbf4717c5eac4fc2f6c67f137eea9fd626ae
AnushaAR20 <167959589+AnushaAR20@users.noreply.github.com>
2024-08-20 11:02:35 +0530

    Update typewise-alert.tests.cpp

--- typewise-alert.tests.cpp
+++ typewise-alert.tests.cpp
@@ -5,6 +5,13 @@ TEST(TypeWiseAlertTestSuite,InfersBreachAccordingToLimits) {
 EXPECT_EQ(inferBreach(12, 10, 20), NORMAL);
 EXPECT_EQ(inferBreach(9, 10, 20), TOO_LOW);
 EXPECT_EQ(inferBreach(21, 10, 20), TOO_HIGH);
-EXPECT_EQ(inferBreach(21, 10, 21), TOO_HIGH);
+EXPECT_EQ(inferBreach(21, 10, 21), NORMAL);
 
+}
+
+TEST(TypeWiseAlertTestSuite, ClassifiesTemperatureBreachForPassiveCooling) {
+  EXPECT_EQ(classifyTemperatureBreach(PASSIVE_COOLING, 30), NORMAL);
+  EXPECT_EQ(classifyTemperatureBreach(PASSIVE_COOLING, -1), TOO_LOW);
+  EXPECT_EQ(classifyTemperatureBreach(PASSIVE_COOLING, 36), TOO_HIGH);
+  EXPECT_EQ(classifyTemperatureBreach(PASSIVE_COOLING, 35), NORMAL);
 }
